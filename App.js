214a3ac8570bf2a154c4fc865a58c0767ec324d5
Ruben Robles <ruben.robles@uabc.edu.mx>
2022-01-11 17:35:41 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import JokeTemplate from './JokeTemplate';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <JokeTemplate />
+    </div>
+  );
+}
+
+export default App;
